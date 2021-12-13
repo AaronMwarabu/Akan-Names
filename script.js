863882d30d5cf3a5c74768f778e304150a356891
Aaron Mwarabu <mwarabuaaron01@gmail.com>
2021-12-13 22:40:56 +0300

    add script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function check_birthday() {
-    let gender = document.querySelector("#Gender").value;
+window.addEventListener("DOMContentLoaded", function() {
+    console.log("document loaded");
 
-    let dates = document.querySelector("#date").value;
-    let calendarDate = new dates(dates);
-    let birthday = calendarDate.getDay();
+    let dateForm = document.querySelector("#dateForm");
+    console.log("this is our form", dateForm);
 
-    let male = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    
-}
+    dateForm.addEventListener("submit", function(submitEvent) {
+        submitEvent.preventDefault();
+        console.log("submitted");
+
+        let gender = document.querySelector("#Gender").value;
+
+        let dates = document.querySelector("#date").value;
+        let calendarDate = new Date(dates);
+        let birthday = calendarDate.getDay();
+        
+        let male = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        let female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        if (gender == "male") {
+            alert(male[birthday]);
+        } else if (gender == "female") {
+            alert(female[birthday]);
+        } else {
+            alert("error input your gender");
+        }
+    })
+})
+
